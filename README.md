2368fb21fb8ac4e3d38a9effbf2d7ba36032dc26
Antonio <suazoantonio67@gmail.com>
2024-03-09 23:17:21 -0300

    agregado celsius a kelvin

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # calculadora-grados
  calculadora de grados
+ de celsius a fahrenheit
+ de celsius a kelvin
+ de fahrenheit a kelvin
+ de fahrenheit a celsius
+ de kelvin a fahrenheit
+ de kelvin a celsius
---
+++ celsius-kelvin.py
@@ -0,0 +1,6 @@
+# Titulo
+print("Calculadora de Grados Celsius a Grados Kelvin")
+
+celsius = int(input("Ingrese sus grados celsius: "))
+kelvin = celsius + 273
+print("Son " + str(kelvin) + " grados kelvin")
